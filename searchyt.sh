c0db61dd554c7307dcc0b99f1a967480f0c10873
Necronoise <necronoise@hotmail.com>
2018-06-28 22:13:30 -0300

    Youtube-dl Search Music

--- searchyt.sh
+++ searchyt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #by necronoise (thanks fenix);
-#you will need youtube-dl and mpv
+#make sure you have youtube-dl and mpv
+#this is a simple script thats allow you to search and listen music in your terminal
+
 echo "Youtube-dl.";
 
 read -p "Pesquise por sua musica: " musica;
