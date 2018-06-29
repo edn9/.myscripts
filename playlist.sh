c7914f92eebce3b8ec9db75f188974971d0567c0
Necronoise <necronoise@hotmail.com>
2018-06-28 22:09:37 -0300

    Youtube-dl Playlist

--- playlist.sh
+++ playlist.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #made by necronoise
+#make sure you have mpv and youtube-dl in your distro
 echo Minha Playlist - Necronoise on Youtube && mpv https://www.youtube.com/playlist?list=LLoUEOHSv_q4uhK37FIZImGw --no-video --load-unsafe-playlists 
