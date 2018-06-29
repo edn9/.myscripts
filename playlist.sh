dadb2eee28f11e431dcf2fbe9bc06a51cdf769ba
Necronoise <necronoise@hotmail.com>
2018-06-28 21:42:52 -0300

    Update playlist.sh

--- playlist.sh
+++ playlist.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#made by necronoise
 echo Minha Playlist - Necronoise on Youtube && mpv https://www.youtube.com/playlist?list=LLoUEOHSv_q4uhK37FIZImGw --no-video --load-unsafe-playlists 
