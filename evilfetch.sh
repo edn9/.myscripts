c776f990187d53b2cf721d4d6082563827fbcb61
Necronoise <necronoise@hotmail.com>
2018-06-28 22:08:37 -0300

    Neofetch Config

--- evilfetch.sh
+++ evilfetch.sh
@@ -1,3 +1,4 @@
 #made by necronoise
 #in --w3m put the /path/of/img you want to show in your neofetch
+#make sure you have neofetch and w3m in your distro
 clear && neofetch --w3m /home/noise/imagens/wallpapers/halo.png && sleep 1d
